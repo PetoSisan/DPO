e9867773f1638543f1286a5249a1b93f65d3163c
Peter Šišan <peter.sisan0@gmail.com>
2026-01-28 15:13:29 +0100

    refactoring of `fill` to use dictionaries instead of if branches

--- filler.py
+++ filler.py
@@ -5,71 +5,80 @@ from Project import Project
 
 INCOMPLETE_DATA = "Ostatné údaje nie sú dostupné v žiadosti ani v PD."
 
+def fill(row, cell: int, header_data: dict[str, str], data: dict[str, str]) -> None:
+    curr_cell = row.cells[cell]
+    text = curr_cell.text.strip()
 
-def fill(doc: Document, cell, applicants: list[Person], project_owners: list[Person],
-         project: Project, table: int, row: int, c: int) -> None:
-    text = cell.text.strip()
-
-    if text == "Meno Priezvisko":
-        cell.text = ""
-        for applicant in applicants:
-            cell.text += applicant.get_full_name()
+    if header_data.get(text) is not None:
+        curr_cell.text = header_data[text]
     
-    elif text == "Ulica číslo":
-        cell.text = ""
-        for applicant in applicants:
-            cell.text += f"{applicant.address.street} {applicant.address.building_number}"
-
-    elif text == "PSČ Mesto":
-        cell.text = ""
-        for applicant in applicants:
-            cell.text += f"{applicant.address.postal_code} {applicant.address.city}"
+    if data.get(text) is not None:
+        next_cell = row.cells[cell + 1]
+        next_cell.text = data[text]
     
-    elif text == "Žiadateľ":
-        next_cell = doc.tables[table].rows[row].cells[c + 1]
-        next_cell.text = ""
-        for applicant in applicants:
-            next_cell.text += applicant.to_string()
-
-            if not applicant.is_complete():
-                next_cell.text += f"\n{INCOMPLETE_DATA}\n"
-            
-    
-    elif text == "Stavebník":
-        next_cell = doc.tables[table].rows[row].cells[c + 1]
-        next_cell.text = ""
-        for project_owner in project_owners:
-            next_cell.text += project_owner.to_string()
-
-            if not project_owner.is_complete():
-                next_cell.text += f"\n{INCOMPLETE_DATA}\n"
-    
-    elif "ID stavby" in cell.text:
-        next_cell = doc.tables[table].rows[row].cells[c + 1]
-        next_cell.text = project.id
-    
-    elif text == "Názov stavby":
-        next_cell = doc.tables[table].rows[row].cells[c + 1]
-        next_cell.text = project.title
-
-    elif text == "Identifikačné údaje stavby":
-        next_cell = doc.tables[table].rows[row].cells[c + 1]
-        next_cell.text = project.format_parcels()
-    
-    elif text == "Členenie stavby":
-        next_cell = doc.tables[table].rows[row].cells[c + 1]
-        next_cell.text = project.format_facilities()
+    return
 
 
+def prepare_applicants(applicants: list[Person],
+                       header_data: dict[str, str],
+                       data: dict[str, str]) -> None:
+    for applicant in applicants:
+        header_data["Meno Priezvisko"] = \
+            header_data.get("Meno Priezvisko", "") + f"{applicant.get_full_name()}"
+        
+        header_data["Ulica číslo"] = \
+            header_data.get("Ulica číslo", "") + f"{applicant.address.street} {applicant.address.building_number}"
+        
+        header_data["PSČ Mesto"] = \
+            header_data.get("PSČ Mesto", "") + f"{applicant.address.postal_code} {applicant.address.city} \n"
+        
+        data["Žiadateľ"] = \
+            data.get("Žiadateľ", "") + f"{applicant.to_string()} \n"
+        
+        if not applicant.is_complete():
+             data["Žiadateľ"] += f"\n{INCOMPLETE_DATA}\n"
+    return
+
+
+def prepare_project_owners(project_owners: list[Person],
+                           data: dict[str, str]) -> None:
+    for project_owner in project_owners:
+        data["Stavebník"] = \
+            data.get("Stavebník", "") + f"{project_owner.to_string()} \n"
+        
+        if not project_owner.is_complete():
+             data["Stavebník"] += f"\n{INCOMPLETE_DATA}\n"
+
+
+def prepare_project(project: Project, data: dict[str, str]) -> None:
+    data["ID stavby \n(ak bolo pridelené Portálom výstavby)"] = project.id
+    data["Názov stavby"] = project.title
+    data["Identifikačné údaje stavby"] = project.format_parcels()
+    data["Členenie stavby"] = project.format_facilities()
+    return
+
+
+def prepare_data(applicants: list[Person],
+                 project_owners: list[Person],
+                 project: Project) -> tuple[dict[str, str], dict[str, str]]:
+    header_data: dict[str, str] = {}
+    data: dict[str, str] = {}
+
+    prepare_applicants(applicants, header_data, data)
+    prepare_project_owners(project_owners, data)
+    prepare_project(project, data)
+
+    return header_data, data
 
 def fill_doc(file_name: str, applicants: list[Person], project_owners: list[Person],
              project: Project, timestamp: str) -> list[str]:
-    doc = Document(file_name)   
+    doc = Document(file_name)
+    header_data, data = prepare_data(applicants, project_owners, project)
 
-    for t, table in enumerate(doc.tables):
-        for r, row in enumerate(table.rows):
-            for c, cell in enumerate(row.cells):
-                fill(doc, cell, applicants, project_owners, project, t, r, c)
+    for table in doc.tables:
+        for row in table.rows:
+            for c in range(len(row.cells)):
+                fill(row, c, header_data, data)
     
     doc.paragraphs[len(doc.paragraphs) - 1].text = f"Edited by script on {timestamp}"
     run = doc.paragraphs[len(doc.paragraphs) - 1].runs[0]
