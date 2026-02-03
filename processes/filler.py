86d5536731602585b896a7d8072af3cb1108c4bc
Peto Šišan <peter.sisan0@gmail.com>
2026-02-03 15:15:28 +0100

    refactoring + tidying the code before Form implementation

--- processes/filler.py
+++ processes/filler.py
@@ -2,8 +2,7 @@ from docx import Document
 from docx.shared import RGBColor
 from entities.Person import Person
 from entities.Project import Project
-
-INCOMPLETE_DATA = "Ostatné údaje nie sú dostupné v žiadosti ani v PD."
+from processes.parser import prepare_data
 
 def fill(row, cell: int, header_data: dict[str, str], data: dict[str, str]) -> None:
     curr_cell = row.cells[cell]
@@ -19,61 +18,8 @@ def fill(row, cell: int, header_data: dict[str, str], data: dict[str, str]) -> N
     return
 
 
-def prepare_applicants(applicants: list[Person],
-                       header_data: dict[str, str],
-                       data: dict[str, str]) -> None:
-    for applicant in applicants:
-        header_data["Meno Priezvisko"] = \
-            header_data.get("Meno Priezvisko", "") + f"{applicant.get_full_name()}"
-        
-        header_data["Ulica číslo"] = \
-            header_data.get("Ulica číslo", "") + f"{applicant.address.street} {applicant.address.building_number}"
-        
-        header_data["PSČ Mesto"] = \
-            header_data.get("PSČ Mesto", "") + f"{applicant.address.postal_code} {applicant.address.city} \n"
-        
-        data["Žiadateľ"] = \
-            data.get("Žiadateľ", "") + f"{applicant.to_string()} \n"
-        
-        if not applicant.is_complete():
-             data["Žiadateľ"] += f"\n{INCOMPLETE_DATA}\n"
-    return
-
-
-def prepare_project_owners(project_owners: list[Person],
-                           data: dict[str, str]) -> None:
-    for project_owner in project_owners:
-        data["Stavebník"] = \
-            data.get("Stavebník", "") + f"{project_owner.to_string()} \n"
-        
-        if not project_owner.is_complete():
-             data["Stavebník"] += f"\n{INCOMPLETE_DATA}\n"
-
-
-def prepare_project(project: Project, data: dict[str, str]) -> None:
-    data["ID stavby \n(ak bolo pridelené Portálom výstavby)"] = project.id
-    data["Názov stavby"] = project.title
-    data["Identifikačné údaje stavby"] = project.format_parcels()
-    data["Členenie stavby"] = project.format_facilities()
-    return
-
-
-def prepare_data(applicants: list[Person],
-                 project_owners: list[Person],
-                 project: Project) -> tuple[dict[str, str], dict[str, str]]:
-    header_data: dict[str, str] = {}
-    data: dict[str, str] = {}
-
-    prepare_applicants(applicants, header_data, data)
-    prepare_project_owners(project_owners, data)
-    prepare_project(project, data)
-
-    return header_data, data
-
-def fill_doc(file_name: str, applicants: list[Person], project_owners: list[Person],
-             project: Project, timestamp: str) -> list[str]:
+def fill_doc(file_name: str, header_data: dict[str, str], data: dict[str, str], timestamp: str) -> list[str]:
     doc = Document(file_name)
-    header_data, data = prepare_data(applicants, project_owners, project)
 
     for table in doc.tables:
         for row in table.rows:
@@ -84,5 +30,3 @@ def fill_doc(file_name: str, applicants: list[Person], project_owners: list[Pers
     run = doc.paragraphs[len(doc.paragraphs) - 1].runs[0]
     run.font.color.rgb = RGBColor(255, 255, 255)
     doc.save(file_name)
-
-
