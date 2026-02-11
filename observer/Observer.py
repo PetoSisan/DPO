f4da30cb67257c5f43ec8861149602d9df1dd646
Peto Šišan <peter.sisan0@gmail.com>
2026-02-11 16:14:05 +0100

    Observer.py - adds full implementation (for now) of finish() callback

--- observer/Observer.py
+++ observer/Observer.py
@@ -5,7 +5,8 @@ from ..form.Form import IForm
 from ..form.FormState import FormState
 
 from processes.filler import fill_doc
-from processes.loger import log
+from processes.loger import record
+from processes.loger import find_missing_data
 from processes.parser import prepare_data
 
 from entities.Person import Person
@@ -31,11 +32,24 @@ class Observer:
         """Callback"""
         new_doc_name = f"{self.project.id} - DPO ({self.timestamp}).docx"
 
+        if state == FormState.ABORTED:
+            print("Vypĺňanie formulára bolo prerušené.")
+
         qna = {row_name: str.join(answers) for row_name, answers in qna.items()}
         header_data, data = prepare_data(self.applicants, self.project_owners, self.project, qna)
         
-        # try:
-        #     copy(self.template_name, new_doc_name)
-        #     fill_doc(new_doc_name, header_data, data, self.timestamp)
-            
-        # except 
+        error = ""
+        try:
+            copy(self.template_name, new_doc_name)
+            fill_doc(new_doc_name, header_data, data, self.timestamp)
+
+        except FileNotFoundError as e:
+            error = "Súbor sa nenašiel. Prosím skontrolujte, či zadaný súbor existuje v pracovnom adresári." \
+                f" Vyhodená chyba: \n {str(e)} \n"
+    
+        except Exception as e:
+            error = f"Počas behu programu sa objavila neočakávaná chyba :(. Vyhodená chyba: \n {str(e)} \n"
+
+        record(error, self.timestamp, new_doc_name, self.project.id,
+               find_missing_data(self.applicants, self.project_owners))
+        return True if error == "" else False
