ee0c8ad6ef0bdb6a9cf45776483e44de44f88534
Peto Šišan <peter.sisan0@gmail.com>
2026-02-20 17:22:26 +0100

    fix referencing of qna from form to manager + moves form state to form

--- form/Form.py
+++ form/Form.py
@@ -1,4 +1,5 @@
 from form.Question import Question
+from form.FormState import FormState
 
 Answer = str
 
@@ -6,4 +7,5 @@ class Form:
     def __init__(self, start: Question):
         self.start = start
         self.qna: dict[str, list[Answer]] = {}
+        self.state = FormState.NOT_STARTED
     
