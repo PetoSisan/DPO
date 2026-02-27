7babfd1acc77e59c196309c5c9112c13b9e28c77
Peto Šišan <peter.sisan0@gmail.com>
2026-02-27 11:41:33 +0100

    adds view and viewmanager

---
+++ GUI/views/ViewManager.py
@@ -0,0 +1,83 @@
+from PySide6.QtWidgets import QWidget, QStackedWidget, QVBoxLayout
+
+from View import View
+
+from GUI.GUIFormManager import GUIFormManager
+
+
+
+# class MainWindow(QWidget):
+#     def __init__(self, formMananger: GUIFormManager):
+#         super().__init__()
+#         self.setWindowTitle("Fomulár k DPO")
+#         self.resize(1920, 1080)
+
+#         self.formManager = formMananger
+#         self.stack = QStackedWidget()
+        
+        
+#         # self.stack.addWidget(self.sign_in_view)
+#         # self.stack.addWidget(self.sign_up_view)
+
+#         self.connect()
+
+#         # self.stack.setCurrentWidget(self.sign_in_view)
+
+#         layout = QVBoxLayout(self)
+#         layout.addWidget(self.stack)
+
+
+#     def connect(self) -> None:
+#         # self.sign_in_view.switch_to_sign_up.connect(lambda: self.stack.setCurrentWidget(self.sign_up_view))
+#         # self.sign_up_view.switch_to_sign_in.connect(lambda: self.stack.setCurrentWidget(self.sign_in_view))
+#         # self.sign_in_view.sign_in.connect(self.sign_in)
+#         # self.sign_up_view.sign_up.connect(self.sign_up)
+#         pass
+    
+
+
+#     def redirect(self, old: View | None, new: View, msg: str= "") -> None:   
+#         if old is not None:
+#             self.stack.removeWidget(old)
+
+#         self.stack.addWidget(new)
+#         self.stack.setCurrentWidget(new)
+
+#         if len(msg) != 0:
+#             new.success(msg)
+
+
+class ViewManager():
+    def __init__(self, formMananger: GUIFormManager):
+        self.formManager = formMananger
+        self.stack = QStackedWidget()
+        
+        
+        # self.stack.addWidget(self.sign_in_view)
+        # self.stack.addWidget(self.sign_up_view)
+
+
+        # self.stack.setCurrentWidget(self.sign_in_view)
+
+        # layout = QVBoxLayout(self)
+        # layout.addWidget(self.stack)
+
+
+    def connect(self) -> None:
+        # self.sign_in_view.switch_to_sign_up.connect(lambda: self.stack.setCurrentWidget(self.sign_up_view))
+        # self.sign_up_view.switch_to_sign_in.connect(lambda: self.stack.setCurrentWidget(self.sign_in_view))
+        # self.sign_in_view.sign_in.connect(self.sign_in)
+        # self.sign_up_view.sign_up.connect(self.sign_up)
+        pass
+    
+
+
+    def redirect(self, old: View | None, new: View, msg: str= "") -> None:   
+        if old is not None:
+            self.stack.removeWidget(old)
+
+        self.stack.addWidget(new)
+        self.stack.setCurrentWidget(new)
+
+        if len(msg) != 0:
+            new.success(msg)
