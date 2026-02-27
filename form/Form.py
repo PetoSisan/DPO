63c7984ef67cf10142ec56ff4f6b3c259007de0a
Peto Šišan <peter.sisan0@gmail.com>
2026-02-27 11:40:55 +0100

    adds travering ofbetween questions

--- form/Form.py
+++ form/Form.py
@@ -5,7 +5,20 @@ Answer = str
 
 class Form:
     def __init__(self, start: Question):
-        self.start = start
+        self.current: Question | None = start
         self.qna: dict[str, list[Answer]] = {}
         self.state = FormState.NOT_STARTED
+    
+
+    def add_answers(self, question: str, answers: list[str]) -> None:
+        self.qna[question] = answers
+
+
+    def get_current_question(self) -> Question | None:
+        return self.current
+    
+
+    def next_question(self) -> Question | None:
+        self.current = self.current.next()
+        return self.current
     
