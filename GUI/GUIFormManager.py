5cff33bff069016fbf7974c36ec1d02696b6eea0
Peto Šišan <peter.sisan0@gmail.com>
2026-03-23 09:36:24 +0100

    implements GUIFormManager.run()

--- GUI/GUIFormManager.py
+++ GUI/GUIFormManager.py
@@ -1,18 +1,19 @@
-from form.IFormManager import IFormManager
 from PySide6.QtWidgets import QApplication
 
 from form.Form import Form
+from form.IFormManager import IFormManager
+
+from GUI.views.Window import Window
 
 class GUIFormManager(IFormManager):
     def __init__(self, form: Form):
         super().__init__(form)
     
     def run(self):
-        pass
-        # self.app = QApplication([])
-        # self.window = QWidget()
-        # self.window.show()
-        # self.app.exec()
+        self.app = QApplication([])
+        self.window = Window()
+        self.window.show()
+        self.app.exec()
     
 
     def quit(self) -> None:
