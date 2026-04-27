80fea5d971f0c34f326811f8c84ed9fbf474e62e
Peto Šišan <peter.sisan0@gmail.com>
2026-04-27 09:59:50 +0200

    adds multiple choice flag to question

--- form/Question.py
+++ form/Question.py
@@ -1,10 +1,12 @@
 from __future__ import annotations # forward references for self-referential data structures
 
 class Question:
-    def __init__(self, title: str, row_name: str, answers: dict[str, Question | None]):
+    def __init__(self, title: str, row_name: str, answers: dict[str, Question | None], multiple_choice: bool):
         self.title = title
         self.row_name = row_name
         self.answers = answers
+        self.multiple_choice = multiple_choice
+
 
     def next(self, answer: str) -> Question | None:
         return self.answers.get(answer)
