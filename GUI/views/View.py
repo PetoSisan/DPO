02b505b4c9c5d286d23692ee1756a91979d0c253
Peto Šišan <peter.sisan0@gmail.com>
2026-05-14 14:41:52 +0200

    removes style sheet

--- GUI/views/View.py
+++ GUI/views/View.py
@@ -1,25 +1,26 @@
 import os
 from pathlib import Path
 
+from typing import Callable
 
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QMessageBox, QTableWidget, QTableWidgetItem, QPushButton
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QMessageBox
 from PySide6.QtCore import QFile, Qt
 from PySide6.QtUiTools import QUiLoader
 from GUI.style import base
 
+from form.Question import Question
 
 class View(QWidget):
     def __init__(self, design_file: str, parent: QWidget | None = None):
         super().__init__(parent)
         self.load_ui(design_file)
-        self.setStyleSheet(base())
         self.error_label = None
         self.resize(1920, 1080)
 
 
     def load_ui(self, design_file: str):
         loader = QUiLoader()
-        path = os.fspath(Path(__file__).resolve().parent / design_file)
+        path = os.fspath(Path(__file__).resolve().parent / "design" /  design_file)
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
         self.ui = loader.load(ui_file)
