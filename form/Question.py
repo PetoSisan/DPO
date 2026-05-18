2ba8ae6bd174f5091f34e3a8e165ffa8b883491f
Peter Šišan <peter.sisan0@gmail.com>
2026-05-18 12:30:13 +0200

    removes multiple choice flag - replace with abstract class instead

--- form/Question.py
+++ form/Question.py
@@ -1,11 +1,13 @@
 from __future__ import annotations # forward references for self-referential data structures
 
-class Question:
-    def __init__(self, title: str, row_name: str, answers: dict[str, Question | None], multiple_choice: bool):
+from abc import ABC
+
+
+class Question(ABC):
+    def __init__(self, title: str, row_name: str, answers: dict[str, Question | None]):
         self.title = title
         self.row_name = row_name
         self.answers = answers
-        self.multiple_choice = multiple_choice
 
 
     def next(self, answer: str) -> Question | None:
