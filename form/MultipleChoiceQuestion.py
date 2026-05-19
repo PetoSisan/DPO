1ef8d0ebb23d1bfc641c632219f12c6d5d8f30bb
Peter Šišan <peter.sisan0@gmail.com>
2026-05-19 10:57:31 +0200

    adds types to init

--- form/MultipleChoiceQuestion.py
+++ form/MultipleChoiceQuestion.py
@@ -1,5 +1,5 @@
 from form.Question import Question
 
 class MultipleChoiceQuestion(Question):
-    def __init__(self, title, row_name, answers):
+    def __init__(self, title: str, row_name: str, answers: dict[str, Question | None]):
         super().__init__(title, row_name, answers)
