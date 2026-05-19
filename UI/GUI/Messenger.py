b061b7c97acb9e8fae6bcd9302c1e5b3ef3784bd
Peter Šišan <peter.sisan0@gmail.com>
2026-05-19 15:07:30 +0200

    adds messenger

---
+++ UI/GUI/Messenger.py
@@ -0,0 +1,24 @@
+from typing import Callable
+from form.Question import Question
+from form.Form import Form
+
+class Messenger:
+    def __init__(self, form: Form, quit: Callable[[], None],
+                 new_view: Callable[[], None] | None = None):
+        self.form = form
+        self.new_view = new_view
+        self.quit_callback = quit
+
+    def get_question(self) -> Question | None:
+        return self.form.get_current_question()
+    
+    def add_answers(self, answers: list[str]):
+        self.form.add_answers(answers)
+        self.form.next_question(answers)
+        self.new_view()
+
+    def quit(self) -> None:
+        return self.quit_callback()
+    
+    def register_callback(self, new_view: Callable[[], None]) -> None:
+        self.new_view = new_view
