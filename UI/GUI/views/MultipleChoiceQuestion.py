5758c670ac7fe83b48768b95ab008f5684418e2c
Peter Šišan <peter.sisan0@gmail.com>
2026-05-19 15:57:52 +0200

    fix in naming: view suffix was missing

--- UI/GUI/views/MultipleChoiceQuestion.py
+++
@@ -1,8 +0,0 @@
-from UI.GUI.views.QuestionView import QuestionView
-from form.Question import Question
-
-from typing import Callable
-
-class MultipleChoiceQuestionView(QuestionView):
-    def __init__(self, q: Question, send_answers: Callable[[list[str]], None]):
-        super().__init__("multiple_choice_question.ui", q, send_answers)
