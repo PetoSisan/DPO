097cfcee84d4491a9273590c90a5ef615cfbc64d
Peter Šišan <peter.sisan0@gmail.com>
2026-05-19 15:58:36 +0200

    adds qna to summary view for displaying results

--- UI/GUI/views/SummaryView.py
+++ UI/GUI/views/SummaryView.py
@@ -1,5 +1,6 @@
 from UI.GUI.views.View import View
 
 class SummaryView(View):
-    def __init__(self):
+    def __init__(self, qna: dict[str, list[str]]):
         super().__init__("summary.ui")
+        self.qna = qna
