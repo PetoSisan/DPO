caf774fe83ec1b522c5bc78d3470515eb34912ac
Peto Šišan <peter.sisan0@gmail.com>
2026-05-27 11:26:41 +0200

    feat: adds implementation of loading answers in question view

--- UI/GUI/views/MultipleChoiceQuestionView.py
+++ UI/GUI/views/MultipleChoiceQuestionView.py
@@ -3,6 +3,15 @@ from form.Question import Question
 
 from typing import Callable
 
+from PySide6.QtWidgets import QCheckBox
+
 class MultipleChoiceQuestionView(QuestionView):
     def __init__(self, q: Question, send_answers: Callable[[list[str]], None]):
         super().__init__("multiple_choice_question.ui", q, send_answers)
+    
+
+    def load_answers(self):
+        # Add new radio buttons from data
+        for row, answer in enumerate(self.question.answers.keys()):
+            check_box = QCheckBox(answer)
+            self.ui.gridLayout.addWidget(check_box, row, 0)
--- UI/GUI/views/SingleChoiceQuestionView.py
+++ UI/GUI/views/SingleChoiceQuestionView.py
@@ -3,6 +3,14 @@ from form.Question import Question
 
 from typing import Callable
 
+from PySide6.QtWidgets import QRadioButton
+
 class SingleChoiceQuestionView(QuestionView):
     def __init__(self, q: Question, send_answers: Callable[[list[str]], None]):
         super().__init__("single_choice_question.ui", q, send_answers)
+
+    def load_answers(self):
+        # Add new radio buttons from data
+        for row, answer in enumerate(self.question.answers.keys()):
+            radio_button = QRadioButton(answer)
+            self.ui.gridLayout.addWidget(radio_button, row, 0)
